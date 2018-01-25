5ecc7df606b72b3d0482a1983113fe5d0165e7a6
JPB <joann.brereton@gmail.com>
2018-01-25 09:46:32 -0500

    See what this does to the running service...
    ..on Fargate ECS.

--- src/index.php
+++ src/index.php
@@ -18,6 +18,7 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS edited by JoAnn to show automatic deployment.</p>
+                <p>Another change to see what it does to the running ECS Fargate service.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
